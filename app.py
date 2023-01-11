12aa5eee0e828ce852deeae8dbf843c161eda8a6
Kenneth John Locey <ken@weecology.org>
2023-01-11 09:58:53 -0600

    updated app data files -KL

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
 import dash_bootstrap_components as dbc
-#from dash.exceptions import PreventUpdate
 
 import pandas as pd
 import plotly.graph_objects as go
@@ -13,16 +12,10 @@ import sys
 import re
 import csv
     
-#from urllib.request import urlopen
 import urllib
-#from math import isnan
 import numpy as np
 import statsmodels.api as sm
 from scipy import stats
-
-#warnings.filterwarnings('ignore')
-#pd.set_option('display.max_columns', None)
-
 
 px.set_mapbox_access_token('pk.eyJ1Ijoia2xvY2V5IiwiYSI6ImNrYm9uaWhoYjI0ZDcycW56ZWExODRmYzcifQ.Mb27BYst186G4r5fjju6Pw')
 ################################# CONFIG APP #################################
